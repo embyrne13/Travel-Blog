cd1c70e7c05e6aac0f3e27ba234f18f5c430463b
Emily Byrne <embyrne13@gmail.com>
2022-07-22 21:19:54 -0400

    comment schema

--- models/index.js
+++ models/index.js
@@ -1,14 +1,14 @@
 const mongoose = require('mongoose')
-// const commentSchema = require('./comment')
+const commentSchema = require('./comment')
 // const formSchema = require('./form')
 const contentSchema = require('./content')
 
-// const Comment = mongoose.model('Comment', commentSchema)
+const Comment = mongoose.model('Comment', commentSchema)
 // const Form = mongoose.model('Form', contentSchema)
 const Content = mongoose.model('Content', contentSchema)
 
 module.exports = {
-  // Comment,
+  Comment,
   Content
   // Form
 }
