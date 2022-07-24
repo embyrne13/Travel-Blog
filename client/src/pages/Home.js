07dd17c7be8ba350f6f30b9798b6aba51e76a0b8
Emily Byrne <embyrne13@gmail.com>
2022-07-23 22:43:10 -0400

    useEffect and state content

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,7 +2,6 @@ const Home = () => {
   return (
     <div>
       <h1> HELLO </h1>
-      {/* <img src="https://thumbs.dreamstime.com/z/round-pin-map-marker-pointer-sign-gps-location-flat-symbol-text-you-here-stock-vector-164843221.jpg" /> */}
     </div>
   )
 }
