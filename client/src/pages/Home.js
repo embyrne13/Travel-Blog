b0630bfb1aa364df66c640c0cfb2783f986e4ff4
Emily Byrne <embyrne13@gmail.com>
2022-07-23 22:34:27 -0400

    finally connected databases

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,7 +1,8 @@
 const Home = () => {
   return (
     <div>
-      <h1> hello </h1>
+      <h1> HELLO </h1>
+      {/* <img src="https://thumbs.dreamstime.com/z/round-pin-map-marker-pointer-sign-gps-location-flat-symbol-text-you-here-stock-vector-164843221.jpg" /> */}
     </div>
   )
 }
