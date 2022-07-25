2b22132889d281253a30685a8c24c97b28364510
Emily Byrne <embyrne13@gmail.com>
2022-07-25 16:12:08 -0400

    add a comment works!

--- client/src/components/City.jsx
+++ client/src/components/City.jsx
@@ -2,7 +2,6 @@ import axios from 'axios'
 import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import Comment from '../components/Comment'
-
 const City = () => {
   let { id } = useParams()
   const [city, setCity] = useState({})
@@ -14,43 +13,13 @@ const City = () => {
   useEffect(() => {
     getCity()
   }, [])
-  const [getc, setGetc] = useState({})
-  const getComment = async () => {
-    const res = await axios.get(`http://localhost:3001/api/content/${id}`)
-    setGetc(res.data)
-  }
 
-  useEffect(() => {
-    getComment()
-  }, [])
-  const [postc, setPostc] = useState({})
-  const postComment = async () => {
-    const res = await axios.post(`http://localhost:3001/api/content/${id}`)
-    setPostc(res.data)
-  }
-
-  useEffect(() => {
-    postComment()
-  }, [])
-
-  const [upc, setUpc] = useState({})
-  const updateComment = async () => {
-    const res = await axios.put(`http://localhost:3001/api/content/${id}`)
-    setUpc(res.data)
-  }
-  useEffect(() => {
-    updateComment()
-  }, [])
-  const [delc, setDelc] = useState({})
-  const deleteComment = async () => {
-    const res = await axios.post(`http://localhost:3001/api/content/${id}`)
-    setDelc(res.data)
-  }
-  useEffect(() => {
-    deleteComment()
-  }, [])
-
-  return <div className="des">{city.description}</div>
+  return (
+    <div>
+      <div className="des">{city.description}</div>
+      <div className="Comment">{<Comment />}</div>
+    </div>
+  )
 }
 
 export default City
--- client/src/components/Comment.jsx
+++ client/src/components/Comment.jsx
@@ -1,72 +1,84 @@
 import React, { useState } from 'react'
 import axios from 'axios'
+import { useEffect } from 'react'
+import { useParams } from 'react-router-dom'
 
-const CommentForm = (props) => {
-  const [name, setName] = useState('')
-  const [city, setCity] = useState('')
+const Comment = (props) => {
+  const [namevalue, setName] = useState('')
   const [comment, setComment] = useState('')
+  const [getc, setGetc] = useState(null)
+  let { id } = useParams()
+  const getComment = async () => {
+    const res = await axios.get(`http://localhost:3001/api/comment/${id}`)
+    setGetc(res.data)
+  }
+  useEffect(() => {
+    getComment()
+  }, [])
 
   const addComment = async (e) => {
-    await axios.post('http://localhost:3001/comment' + props.id, {
-      name: name,
-      city: props.id,
+    e.preventDefault()
+    await axios.post(`http://localhost:3001/api/comment/${id}`, {
+      name: namevalue,
+      city: id,
       comment: comment
     })
-
-    setName('')
+    getComment()
   }
 
-  const changeName = (event) => {
-    let n = event.target.value
-    setName(n)
+  const [upc, setUpc] = useState({})
+  const updateComment = async (comment) => {
+    const res = await axios.put(`http://localhost:3001/api/comment/${comment}`)
+    setUpc(res.data)
+    getComment()
   }
-  const changeCity = (event) => {
-    let n = event.target.value
-    setCity(n)
-  }
-  const changeComment = (event) => {
-    let n = event.target.value
-    setComment(n)
-  }
-
-  const handleSubmit = (e) => {
-    addComment(e)
-    console.lot(props.comment)
+  const [delc, setDelc] = useState({})
+  const deleteComment = async (comment) => {
+    const res = await axios.delete(
+      `http://localhost:3001/api/comment/${comment}`
+    )
+    setDelc(res.data)
+    getComment()
   }
 
   return (
-    <div className="form">
-      <h1>Add A Comment</h1>
-      <div className="input-div">
-        <form onSubmit={handleSubmit}>
-          <div className="name-city-comment">
-            <input
-              type="text"
-              value={name}
-              onChange={changeName}
-              name={'name'}
-              placeholder={'name'}
-            />
-            <input
-              type="text"
-              value={city}
-              onChange={changeCity}
-              name={'city'}
-              placeholder={'city'}
-            />
-            <input
-              type="text"
-              value={comment}
-              onChange={changeComment}
-              name={'comment'}
-              placeholder={'comment'}
-            />
-          </div>
-          <button id="form-submit">Submit</button>
-        </form>
+    <div>
+      {getc?.map((comment) => (
+        <div className="commentinfo">
+          <h2>
+            Name:
+            {comment.name}
+          </h2>
+          <h2>Comment: {comment.comment}</h2>
+          <button onClick={updateComment}>Edit </button>
+          <button onClick={() => deleteComment(comment._id)}>Delete </button>
+        </div>
+      ))}
+      <div className="form">
+        <h1>Add A Comment</h1>
+        <div className="input-div">
+          <form onSubmit={addComment}>
+            <div className="name-comment">
+              <input
+                type="text"
+                value={namevalue}
+                onChange={(e) => setName(e.target.value)}
+                name={'name'}
+                placeholder={'name'}
+              />
+              <input
+                type="text"
+                value={comment}
+                onChange={(e) => setComment(e.target.value)}
+                name={'comment'}
+                placeholder={'comment'}
+              />
+            </div>
+            <button id="form-submit">Submit</button>
+          </form>
+        </div>
       </div>
     </div>
   )
 }
-
-export default CommentForm
+export default Comment
