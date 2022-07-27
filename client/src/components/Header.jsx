e95e843ad4a8cb0f03cc9e7c7733ef801ecc2535
Emily Byrne <embyrne13@gmail.com>
2022-07-27 12:22:04 -0400

    heroku

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -4,7 +4,9 @@ import { Link } from 'react-router-dom'
 const Header = () => {
   return (
     <header>
-      <Link to="/">{<h1 className="title">✈️ EMILY'S TRAVEL TIPS ✈️</h1>}</Link>
+      <Link to="/">
+        {<h1 className="title"> hi ✈️ EMILY'S TRAVEL TIPS ✈️</h1>}
+      </Link>
       <nav>
         <Link className="yo" to="/aboutme">
           About Me
